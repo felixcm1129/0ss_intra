b5581dda8c5c0e1093f2f3aa27f6177b93605523
Felix CarleMilette <1539902@cshawi.ca>
2020-02-24 13:09:27 -0500

    Classe Complétée

--- wpf_intra/intra_models/Customer.cs
+++ wpf_intra/intra_models/Customer.cs
@@ -1,8 +1,21 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
 namespace intra_models
 {
-    public class Customers
+    public class Customers : INotifyPropertyChanged
     {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string ContactInfo { get; set; }
+        public string PicturePath { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
